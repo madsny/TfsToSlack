35ad3f1ef8b040cc9c5d5a42ffdb5b760d9e03cd
Mads Nyborg <nyborg.mads@gmail.com>
2015-09-03 19:08:36 +0200

    Update AppSettings.cs

--- TfsPoller/AppSettings.cs
+++ TfsPoller/AppSettings.cs
@@ -11,8 +11,8 @@ namespace TfsPoller
         public string TfsTeamProjectName => GetAppsettingsString("tfs.teamprojectname");
         public string SlackWebHookUrl => GetAppsettingsString("slack.webhookurl");
         public string SlackUserName => GetAppsettingsString("slack.username") ?? "TFS Build Bot";
-        public string SlackDefaultChannel => GetAppsettingsString("slack.defaultchannel") ?? "@mads";
-        public string SlackFailedChannel => GetAppsettingsString("slack.failedchannel") ?? "@mads";
+        public string SlackDefaultChannel => GetAppsettingsString("slack.defaultchannel") ?? "#general";
+        public string SlackFailedChannel => GetAppsettingsString("slack.failedchannel") ?? "#general";
         public string SlackFailedIcon => GetAppsettingsString("slack.failedicon") ?? ":name_badge:";
         public string SlackDefaultIcon => GetAppsettingsString("slack.defaulticon") ?? ":white_check_mark:";
 
